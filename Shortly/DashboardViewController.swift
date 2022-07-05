f4a0825beffec72f20c1ecf5a72a4e1cba5c4441
Mehrnousch <73842806+Mehrnousch@users.noreply.github.com>
2022-07-05 19:32:28 +0200

    Refactor dashboard view controller

--- Shortly/DashboardViewController.swift
+++ Shortly/DashboardViewController.swift
@@ -9,9 +9,8 @@ import UIKit
 
 class DashboardViewController: UIViewController {
     
-    //linkInputView is appearance of url login
-    var linkInputView = LinkInputView().autoLayoutView()
-    var linkListTableView = LinkListTableView().autoLayoutView()
+    private lazy var linkInputView = LinkInputView().autoLayoutView()
+    private lazy var  linkListTableView = LinkListTableView().autoLayoutView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,67 +30,84 @@ extension DashboardViewController {
         linkInputView.eventHandler = { [weak self] events in
             switch events {
             case .shortLink(let url):
-                self?.getShorterLink(url: url)
+                self?.dismissKeyboard()
+                self?.fetchShorterLink(url: url)
             }
         }
     }
-
 }
 
 // MARK: - API call
 
 extension DashboardViewController {
-
-    private func getShorterLink(url: String) {
-        
+    
+    private func fetchShorterLink(url: String) {
         Task {
             do {
-                let shorterLink = try await ShortlyDataModel.shared.fetchAlbumWithAsyncURLSession(for: url)
-                
-                linkListTableView.linkList.append(shorterLink)
+                let shorterLink = try await NetworkingModel.shared.fetchAlbumWithAsyncURLSession(for: url)
+                    linkListTableView.linkList.append(shorterLink)
             } catch {
                 print("Request failed with error: \(error)")
             }
         }
     }
-    
 }
+
+// MARK: - Keyboard methods
+
+extension DashboardViewController {
     
- 
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+
+   @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+}
+
+// MARK: - setupDefaults
 extension DashboardViewController {
     
     private func setupDefaults() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
         
+      NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        // dismissKeyboard
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+            tap.cancelsTouchesInView = false
+            view.addGestureRecognizer(tap)
     }
     
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+   
     private func setupUI() {
         view.addSubview(linkInputView)
-        //vaghti az Storyboard estefede nemikonim code zir ra minevisim, chera?
         view.addSubview(linkListTableView)
         view.backgroundColor = .secondarySystemBackground
         linkListTableView.backgroundColor = .secondarySystemBackground
     }
-    
-    
-    
-  
+
     private func setupLayout() {
         
         //LinkTableViewCell
         linkListTableView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.size.width).isActive = true
-        //topDashboardView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        linkListTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        linkListTableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
         linkListTableView.bottomAnchor.constraint(equalTo: linkInputView.topAnchor).isActive = true
         
         //linkInputView
         linkInputView.heightAnchor.constraint(equalToConstant: 200 ).isActive = true
         linkInputView.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.size.width).isActive = true
-        //linkInputView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        //linkInputView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         linkInputView.bottomAnchor.constraint(equalTo: view.bottomAnchor) .isActive = true
-        
     }
-    
 }
 
 
