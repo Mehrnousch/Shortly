a0c0846413118b62a1eec83a408e32f255189a02
Mehrnousch <73842806+Mehrnousch@users.noreply.github.com>
2022-07-05 19:31:22 +0200

    Add tableView background view

--- Shortly/Views/LinkListTableView.swift
+++ Shortly/Views/LinkListTableView.swift
@@ -9,16 +9,16 @@ import Foundation
 import UIKit
 
 class LinkListTableView: UIView {
-    private lazy var tableView = UITableView().autoLayoutView()
     
-    var linkList: [ShorterLinkResult] = [] {
+    private lazy var tableView = UITableView().autoLayoutView()
+    private lazy var tableBackgroundView = TableBackgroundView()
+    
+    var linkList: [ShorterLinkData] = [] {
         didSet {
             tableView.reloadData()
         }
     }
-
-
-    //tuye UIviewcontroller ma superDidview?
+    
     override init(frame: CGRect) {
         super.init(frame: .zero)
         setupDefault()
@@ -26,7 +26,6 @@ class LinkListTableView: UIView {
         setupLayout()
     }
     
-    //?
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -36,50 +35,52 @@ class LinkListTableView: UIView {
 // MARK: - TableView Data source
 
 extension LinkListTableView: UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 220
     }
-    
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return linkList.count
+        
+        if linkList.isEmpty {
+            tableView.backgroundView = tableBackgroundView
+            tableView.separatorStyle  = .none
+            return 0
+        }
+        else {
+            tableView.backgroundView  = .none
+            return linkList.count
+        }
     }
     
-    
-    
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         if let cell = tableView.dequeueReusableCell(withIdentifier: "linkCell", for: indexPath) as? LinkTableViewCell {
             let linkData = linkList[indexPath.row]
             cell.setupCell(data: linkData)
+            cell.selectionStyle = .none
            
             cell.eventHandler =  { [weak self] events in
+                
                 switch events {
+                    
                 case .removeItem(let data):
                     if let index = self?.linkList.firstIndex(where: {$0.full_short_link == data.full_short_link}) {
                         self?.linkList.remove(at: index)
-                        self?.tableView.reloadData()
                     }
-                   
                 }
             }
             
             return cell
         }
-        fatalError("could not dequeueReusableCell")
+        return UITableViewCell()
     }
-}
-
-extension LinkListTableView: UITableViewDelegate {
     
-   
 }
-
-
 
 // MARK: - Setup UI
 extension LinkListTableView {
@@ -87,17 +88,13 @@ extension LinkListTableView {
     func setupDefault() {
         tableView.register(LinkTableViewCell.self, forCellReuseIdentifier: "linkCell")
         tableView.dataSource = self
-        tableView.delegate = self
-        // add event UIButton, func press ro call mikone
     }
     
-    
-    
     func setupUI() {
-       addSubview(tableView)
-        tableView.backgroundColor = .secondarySystemBackground
-        // har chizi ke mikhahim dar in laye nemayesh dade beshe bayad dar in ghesmat ezafe konim.addSubView?
-        tableView.backgroundView = UIImageView(image: UIImage(named: "something.png"))
+        addSubview(tableView)
+        addSubview(tableBackgroundView)
+        tableView.backgroundColor = .systemBackground
+        tableBackgroundView.backgroundColor = .systemBackground
     }
     
      private func setupLayout() {
