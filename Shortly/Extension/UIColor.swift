04bc53288b9866591627e8f485514b9257f4b836
Mehrnousch <73842806+Mehrnousch@users.noreply.github.com>
2022-07-05 19:29:57 +0200

    Add UIColor extension

---
+++ Shortly/Extension/UIColor.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor.swift
+//  Shortly
+//
+//  Created by mehrnoush abdinian on 05.07.22.
+//
+
+import Foundation
+import UIKit
+
+public extension UIColor {
+    static let purple = UIColor(named: "purple", in: .main, compatibleWith: nil)
+    static let greenBlue = UIColor(named: "green‌Blue", in: .main, compatibleWith: nil)
+}
